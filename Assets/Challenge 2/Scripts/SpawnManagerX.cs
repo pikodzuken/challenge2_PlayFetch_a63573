924b7fe08d83dff980d2092c8eeda841e6fa42cd
Pi <ines.f.ribeiro12@gmail.com>
2026-03-05 19:03:17 +0000

    passo 8

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,17 +10,23 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
+     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("RandomDelay", startDelay, 3);
     }
-
-    // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+// Randomizing ball spawn time
+void RandomDelay()
+{
+    float randomInterval = Random.Range(0, 3);
+    Invoke("SpawnRandomBall", randomInterval);
+}
+// Spawn random ball at random x position at top of play area
+ void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
          int ballIndex = Random.Range(0, ballPrefabs.Length);
